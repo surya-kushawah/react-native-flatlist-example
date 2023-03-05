0cd9edb1483bb0e655213e3cecd73a1bff3cdc07
surya <spsurya52@gmail.com>
2023-03-05 21:51:01 +0530

    initial project setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, ListRenderItem, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useCallback} from "react";
+import ActorItem, {Actor} from "./components/ActorItem";
+import {listItems} from "./data/Data";
 
 export default function App() {
+  /**
+   *  Avoid arrow functions inline for renderItem
+   */
+  const renderItem: ListRenderItem<Actor> = useCallback(({item}) => (
+    <ActorItem id={item.id} name={item.name} bornAt={item.bornAt} photo={item.photo}/>
+  ), []);
+
+  /**
+   *  Use keyExtractor prop
+   */
+  const keyExtractor = useCallback((items: Actor) => items.id.toString(), [])
+
+  /**
+   * Always use  getItemLayout to optimize FlatList
+   */
+  const ITEM_HEIGHT = 200;
+  const getItemLayout = useCallback((data: any, index: number) => ({
+    length: ITEM_HEIGHT,
+    offset: ITEM_HEIGHT * index,
+    index
+  }), [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>React Native FlatList Example</Text>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={listItems}
+          renderItem={renderItem}
+          keyExtractor={keyExtractor}
+          alwaysBounceVertical={false}
+          getItemLayout={getItemLayout}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:"#eee7e7"
+  },
+  listContainer: {
+    padding: 8,
+    flex: 1,
+  },
+  header: {
+    padding: 8,
+    fontSize: 24,
+    color: "white",
+    lineHeight: 48,
+    backgroundColor: '#ff0000',
   },
 });
---
+++ data/Data.ts
@@ -0,0 +1,52 @@
+import {Actor} from "../components/ActorItem";
+
+export const listItems: Actor[] = [
+
+  {
+    "id": 1,
+    "name": "Robert Downey Jr.",
+    "bornAt": "New York City, NY",
+    "photo": "https://jsonformatter.org/img/Robert-Downey-Jr.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Tom Cruise",
+    "bornAt": "Syracuse, NY",
+    "photo": "https://jsonformatter.org/img/tom-cruise.jpg"
+  },
+
+  {
+    "id": 4,
+    "name": "Morris Kushwaha",
+    "bornAt": "New Delhi City, IN",
+    "photo": "https://androidwave.com/wp-content/uploads/2021/02/android-wave.jpeg"
+  }, {
+    "id": 5,
+    "name": "Scarlett Johansson.",
+    "bornAt": "New York City, NY",
+    "photo": "https://androidwave.com/wp-content/uploads/2021/01/profile_pic-300x300.jpg"
+  },
+  {
+    "id": 7,
+    "name": "Will Smith",
+    "bornAt": "New York City, NY",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaQqg3yvv1Kv3KA2XtPu4T1p4z3j6Ubk-XRzoT5FUJIdkh4RWX6JfMM7Jc&s=0"
+  },
+  {
+    "id": 8,
+    "name": "Leonardo DiCaprio",
+    "bornAt": "New York City, NY",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYZRJI2Hr7MTGsc33GZVm_f0ZLyZiCvHhQk8ojXZxt84pmdzZnHnt5kdld&s=0"
+  },
+  {
+    "id": 6,
+    "name": "Robert Downey Jr.",
+    "bornAt": "New York City, NY",
+    "photo": "https://jsonformatter.org/img/Robert-Downey-Jr.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Tom Cruise",
+    "bornAt": "Syracuse, NY",
+    "photo": "https://jsonformatter.org/img/tom-cruise.jpg"
+  },];
